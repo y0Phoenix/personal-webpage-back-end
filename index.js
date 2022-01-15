e27aeedd0990e1a56f9584c1924534c029ae3fb5
Aaron <aarongraybill3@gmail.com>
2022-01-15 00:40:50 -0600

    start script

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.use('/api/email', require('./routes/Email'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log('server started...')
