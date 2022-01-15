e0ecea81a1fe752b21f40ef5e52911c31321e010
Aaron <aarongraybill3@gmail.com>
2022-01-15 00:35:53 -0600

    changed port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.use('/api/email', require('./routes/Email'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log('server started...')
