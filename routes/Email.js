e0c804a4c1fa8f627e9b14d5478b2947db81f280
Aaron <aarongraybill3@gmail.com>
2022-01-15 16:59:29 -0600

    emailFrom

--- routes/Email.js
+++ routes/Email.js
@@ -12,7 +12,7 @@ router.post('/', [
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
-    const {emailfrom, text, subject} = req.body;
+    const {emailFrom, text, subject} = req.body;
 
     try {
         // create reusable transporter object using the default SMTP transport
@@ -29,10 +29,10 @@ router.post('/', [
 
         // send mail with defined transport object
         let info = await transporter.sendMail({
-            from: emailfrom, // sender address
+            from: emailFrom, // sender address
             to: 'aarongraybill3@gmail.com', // list of receivers
             subject: subject, // Subject line
-            text: `From ${emailfrom}\n\n${text}`, // plain text body
+            text: `From ${emailFrom}\n\n${text}`, // plain text body
         });
 
         console.log("Message sent: %s", info.messageId);
@@ -41,7 +41,7 @@ router.post('/', [
         // Preview only available when sending through an Ethereal account
         console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-        res.json({msg: `Email sent successfully from ${emailfrom}`});
+        res.json({msg: `Email sent successfully from ${emailFrom}`});
                 
     } catch (err) {
         console.error(err.message);
