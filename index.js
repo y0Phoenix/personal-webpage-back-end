9573993c06a1d7145a518562b7605035a1395c64
Aaron <aarongraybill3@gmail.com>
2022-01-15 16:28:36 -0600

    cors middleware

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 app.use(express.json({ extended: false }));
-app.use(express.static('public'));
+app.use(cors());
 
 app.use('/api/email', require('./routes/Email'));
 
